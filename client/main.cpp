36c49d1ed5667ac4437201c16907e53f92a48cd2
moein <moeiiinsh@gmail.com>
2021-04-04 12:46:23 +0430

    client-server basics added

---
+++ client/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define SERVER_PORT 5432
+#define MAX_LINE 256
+
+int
+main()
+{
+    FILE *fp;
+    struct sockaddr_in sin;
+    char buf[MAX_LINE];
+    int s;
+    int len;
+
+    /* build address data structure */
+
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* active open */
+
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    {
+        perror("simplex-talk: connect");
+        close(s);
+        exit(1);
+    }
+
+    /* main loop: get and send lines of text */
+
+    while (fgets(buf, sizeof(buf), stdin)) {
+        buf[MAX_LINE-1] = '\0';
+        len = strlen(buf) + 1;
+        send(s, buf, len, 0);
+    }   
+
+}
